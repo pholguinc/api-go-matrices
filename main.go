f2b5aa6c7185191c57b1eee40e45feb7192daf41
Pedro Holguin <holguin98fx@gmail.com>
2026-05-13 17:32:50 -0500

    feat: Inicializando proyecto

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("API is running")
+	})
+
+	log.Fatal(app.Listen(":3001"))
+}
